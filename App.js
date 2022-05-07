0a992a044e0a88c5c195760241c6e9723148efbb
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-05-07 23:21:25 +0530

    changes done

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
 const score=document.querySelector('.score');
 const startscreen=document.querySelector('.startscreen');
 const gamearea=document.querySelector('.gamearea');
+startscreen.addEventListener('click',start);
+let player={
+    start: false
+}
+let keys={
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft:false,
+    ArrowRight:false
+
+}
+console.log(keys)
 document.addEventListener('keydown',keyaDown)
 document.addEventListener('keyup',keyUp);
 function keyaDown(e){
    e.preventDefault();
-   console.log(e.key)
-
+   keys[e.key]=true;
+//    console.log(keys)
+//    console.log("yes")
 }
 function keyUp(e){
    e.preventDefault();
-   //console.log(e.key)
+   keys[e.key]=false;
 
+}
+function gameplay(){
+    if(player.start){
+        window.requestAnimationFrame(gameplay);
+
+    }
+}
+function start(){
+    gamearea.classList.remove('hide')
+    startscreen.classList.add('hide')
+    player.start=true;
+    window.requestAnimationFrame(gameplay);
+    let car=document.createElement('div');
+    car.setAttribute('class','car')
+    //car.innerText="hey i m ur car buddy";
+    gamearea.appendChild(car)
+     
 }
